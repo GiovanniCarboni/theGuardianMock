c1a81145c33c531c04aec7fc7d22be3598334483
baciladan <baciladan0@gmail.com>
2022-04-10 21:29:47 +0300

    Code review

--- site/script.js
+++ site/script.js
@@ -3,6 +3,8 @@
 const logo = document.querySelector(".logo");
 
 const hideBtn = document.querySelectorAll(".hide-btn");
+// document.getElementById();
+// document.getElementsByClassName();
 const section1 = document.querySelectorAll(".x");
 const section2 = document.querySelectorAll(".y");
 
@@ -11,6 +13,15 @@ hideBtn[0].addEventListener("click", function () {
   if (section1[1].classList.contains("hide")) hideBtn[0].textContent = "Show";
   else hideBtn[0].textContent = "Hide";
 });
+
+// ES 6 Syntax
+// hideBtn[0].addEventListener("click", () => {
+//   section1.forEach((el) => el.classList.toggle("hide"));
+//   if (section1[1].classList.contains("hide")) hideBtn[0].textContent = "Show";
+//   else hideBtn[0].textContent = "Hide";
+// });
+
+// The hide / show feature can be done using HTML and CSS only
 
 hideBtn[1].addEventListener("click", function () {
   section2.forEach((el) => el.classList.toggle("hide"));
